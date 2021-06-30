5dec8883fdae23d6b9f71396fb4743be7aa06356
Salvinus01 <86074085+Salvinus01@users.noreply.github.com>
2021-06-29 21:24:00 -0300

    Initial commit

---
+++ app/src/main/java/br/com/google/google_labs_dice_roller/MainActivity.kt
@@ -0,0 +1,34 @@
+package br.com.google.google_labs_dice_roller
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rowButton: Button = findViewById(R.id.button)
+        rowButton.setOnClickListener { rollDice() }
+    }
+
+    private fun rollDice() {
+        val dice = Dice(6)
+        val diceRow = dice.roll()
+        val resultTextView: TextView = findViewById(R.id.textView)
+        resultTextView.text = diceRow.toString()
+
+        val resultTextView2: TextView = findViewById(R.id.textView3)
+        val dice2 = Dice(20)
+        resultTextView2.text = dice2.roll().toString()
+    }
+}
+
+class Dice(private val numSides: Int) {
+
+    fun roll(): Int {
+        return (1..numSides).random()
+    }
+}
